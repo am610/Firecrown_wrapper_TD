456e90cd58695f1d4d6805a15cbe91971ff2c697
Ayan Mitra <ayanmitra375@gmail.com>
2023-06-12 02:25:53 -0700

    first

---
+++ Firecrown_wrapper.py
@@ -0,0 +1,394 @@
+"""
+python Firecrown_wrapper_example-2.py /global/homes/a/ayanmitr/Analysis/7_CREATE_COV/LSST_BINNED_COV_BBC_SIMDATA_PHOTOZ_1/output hubble_diagram.txt covsys_000.txt.gz sn_only.ini -O $HOME/trash -p "omega_m = 0.3"
+
+
+$SNANA_DEBUG/submit_batch_firecrown/NEW_AYAN_DEBUG-3/dist/Firecrown_wrapper_example-2 /global/homes/a/ayanmitr/Analysis/7_CREATE_COV/LSST_BINNED_COV_BBC_SIMDATA_PHOTOZ_1/output hubble_diagram.txt covsys_000.txt.gz sn_only.ini
+"""
+
+import pandas as pd
+import numpy as np
+import os
+import sys
+import yaml
+import signal
+import subprocess
+from subprocess import Popen
+import datetime
+import logging
+import warnings
+import time
+import argparse
+import pathlib
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter, RawTextHelpFormatter
+from contextlib import contextmanager
+import textwrap
+from FoM import FoM
+
+
+logging.basicConfig(level=logging.INFO)
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+
+
+@contextmanager
+# https://stackoverflow.com/questions/7152762/how-to-redirect-print-output-to-a-file
+def redirected_stdout(outstream):
+    orig_stdout = sys.stdout
+    try:
+        sys.stdout = outstream
+        yield
+    finally:
+        sys.stdout = orig_stdout
+
+
+# Setting up the 'yaml' file
+class CustomHelpFormatter(argparse.ArgumentDefaultsHelpFormatter):
+    def _split_lines(self, text, width):
+        wrapper = textwrap.TextWrapper(width=width)
+        lines = []
+        for line in text.splitlines():
+            if len(line) > width:
+                lines.extend(wrapper.wrap(line))
+            else:
+                lines.append(line)
+        return lines
+
+time0 = time.time()
+now = datetime.datetime.now()
+print("Current Time = ", now)
+dt_string = now.strftime("%d%m%Y_%H%M%S/")
+ENV = os.environ["SNANA_DEBUG"]
+OUTPUT_PATH = (
+    ENV
+    + "/"
+    + "submit_batch_firecrown/NEW_AYAN_DEBUG-3/FIRECROWN_OUTPUT/"  # + dt_string
+)  # 1 This can be an user arg ***
+
+
+usage = "Mandatory arguments required in  fixed order"
+# parser = argparse.ArgumentParser(formatter_class=CustomHelpFormatter, description=usage)
+parser = argparse.ArgumentParser(description=usage)
+parser.add_argument("path", help="Path for the HD and COV")
+parser.add_argument("hd", help="HD ")
+parser.add_argument("cov", help="COV ")
+parser.add_argument("ini", help="*.ini file")
+parser.add_argument(
+    "-O",
+    "--outdir",
+    type=pathlib.Path,
+    default=OUTPUT_PATH,
+    help="-O Output path (Default: %s)" % (OUTPUT_PATH),
+)
+parser.add_argument(
+    "-p",
+    "--param",
+    #    action='store_true',
+    default="",
+    nargs="?",
+    help="-p Override cosmosis parameter values ",
+)
+parser.add_argument(
+    "-s",
+    "--summary",
+    type=pathlib.Path,
+    default=OUTPUT_PATH + "/SUMMARY.YAML",
+    help="-s SUMMARY.YAML output path (Default: %s)" % (OUTPUT_PATH),
+)
+"""
+parser.add_argument(
+    "-i",
+    "--info",
+    type=pathlib.Path,
+    default=COSMOSIS_PATH + "INPUT.INFO",
+    help="-i INPUT.INFO output path (Default: %s)" % (OUTPUT_PATH),
+)
+"""
+args = parser.parse_args()
+
+SUBDIRER = "/ERROR_LOGS/"
+SUBDIR3 = "/COSMOSIS-CHAINS/"
+SUBDIR4 = "/PLOTS"
+OUTPUT_PATH = os.path.expandvars(args.outdir)
+SUMMARY_PATH = os.path.expandvars(args.summary)
+# SUBMIT_PATH = os.path.expandvars(args.info)
+ERROR_PATH = os.path.expandvars(OUTPUT_PATH + SUBDIRER)
+COSMOSIS_PATH = os.path.expandvars(OUTPUT_PATH + SUBDIR3)
+PLOT_PATH = os.path.expandvars(OUTPUT_PATH + SUBDIR4)
+try:
+    os.makedirs(COSMOSIS_PATH)
+except FileExistsError:
+    # directory already exists
+    pass
+SUBMIT_PATH = COSMOSIS_PATH + "INPUT.INFO"
+p = args.param
+
+with open(r"%s" % (SUBMIT_PATH), "w", buffering=1) as OF:
+    with redirected_stdout(OF):
+        print(
+            "\n\nSTAGE 0 = ALL PATH AND FILES IN ARGUMENTS CHECK\nSTAGE 1 = 'generate_sn_data.py'\nSTAGE 2 = COSMOSIS\nSTAGE 3 = POST PROCESSING (PLOT)\n\n"
+        )
+
+    Key = [
+        "STAGE0",
+        "STAGE1",
+        "STAGE2",
+        "STAGE3",
+        "ABORT_IF_ZERO",
+        "FoM",
+        "Ndof",
+        "CPU_MINUTES",
+        "chi2",
+        "sigint",
+        "label",
+        "BLIND",
+        "NWARNINGS",
+        "w0",
+        "w0sig_marg",
+        "wa",
+        "wasig_marg",
+        "OM",
+        "OMsig_marg",
+        "w0ran",
+        "waran",
+        "OMran"
+    ]
+    data = {}
+    # -------------------------------
+
+    # Function Defintions
+
+    # key set to non abort(0) case, guide for sbatch
+    def path_error(path):
+        if os.path.exists(path):
+            data[Key[4]] = 1
+            pass
+        else:
+            data[Key[0]] = "FAILED"
+            data[Key[4]] = 0
+            outputs = yaml.dump(data, file_yaml)
+            raise FileNotFoundError("{0} path does not exist!".format(path))
+
+    def file_error(file):
+        if os.path.isfile(file):
+            data[Key[4]] = 1
+            pass
+        else:
+            data[Key[0]] = "FAILED"
+            data[Key[4]] = 0
+            outputs = yaml.dump(data, file_yaml)
+            raise FileNotFoundError("{0} file does not exist!".format(file))
+
+    # ----------------------------
+
+    """
+        Creating output folders
+    """
+    if not os.path.exists(ERROR_PATH):
+        os.makedirs(ERROR_PATH, exist_ok=True)
+    if not os.path.exists(COSMOSIS_PATH):
+        os.makedirs(COSMOSIS_PATH, exist_ok=True)
+    if not os.path.exists(PLOT_PATH):
+        os.makedirs(PLOT_PATH, exist_ok=True)
+
+    """
+    Checking Files exists
+    """
+    with open(r"%s" % (SUMMARY_PATH), "w", buffering=1) as file_yaml:
+        with redirected_stdout(OF):
+            print("\n#Required Info")
+            print("CWR:", os.getcwd())
+            print("ARG_LIST:", sys.argv)
+            print("TIME_STAMP:", now.strftime("%d-%m-%Y %H:%M:%S"))
+            print("OUTPUT DIR:", OUTPUT_PATH, "\n")
+
+        PWD = os.getcwd()
+        # arg_error(len(sys.argv))
+        path = args.path
+        hd = args.hd
+        cov = args.cov
+        ini = os.path.split(args.ini)[1]
+        # ini_f= args.ini
+        path_error(path)
+        f1 = path + "/" + hd
+        file_error(f1)
+        f2 = path + "/" + cov
+        file_error(f2)
+        data[Key[0]] = "SUCCESS"
+        # ********************
+        # --------------------
+        HD_read = pd.read_csv(f1,comment='#',sep=r'\s+')
+        sacc_path_rm = "rm " + PWD + "/" + "srd-y1-converted.sacc"
+        job0 = subprocess.Popen(sacc_path_rm, stdout=subprocess.PIPE, shell=True)
+
+        # Stage 1
+        # python $FIRECROWN_EXAMPLES_DIR/srd_sn/generate_sn_data.py ${path} ${HD} ${COV}
+        # This assumes that the output  will be a `sacc' file which
+        # will be automatically read in by cosmosis input `ini' file.
+        startTime = time.time()
+        os.environ["PYTHONUNBUFFERED"] = "1"
+        Vector = path + " " + hd + " " + cov
+        with redirected_stdout(OF):
+            print("SACC Input Vector:", Vector)
+        with open(
+            "%sgenerate_sn_data_output_%s.log" % (ERROR_PATH, ini), "wb"
+        ) as gen_file, open(
+            "%sgenerate_sn_data_output_ERROR_%s.err" % (ERROR_PATH, ini), "wb"
+        ) as gen_file_e:
+            job1 = Popen(
+                "python "
+                + "$FIRECROWN_EXAMPLES_DIR/srd_sn/generate_sn_data.py "
+                + Vector,
+                shell=True,
+                text=True,
+                stdout=gen_file,
+                stderr=gen_file_e,
+            )
+        job1.wait()
+        file = open("%sgenerate_sn_data_output_%s.log" % (ERROR_PATH, ini), "a")
+        sys.stdout = file
+        with redirected_stdout(OF):
+            print(job1.stdout)
+        file_e = open("%sgenerate_sn_data_output_ERROR_%s.err" % (ERROR_PATH, ini), "a")
+        sys.stdout = file_e
+        with redirected_stdout(OF):
+            print(job1.stderr)
+        if job1.returncode != 0:
+            os.system('echo "STAGE 1 FAILED *** \nCheck generate_sn_data error logs"')
+            data[Key[1]] = "FAILED"
+            data[Key[4]] = 0
+            outputs = yaml.dump(data, file_yaml)
+            executionTime = np.array(round((time.time() - startTime), 2))
+            with open("%sTimer_STAGE_1.time" % (ERROR_PATH), "w") as f:
+                f.write("%s" % executionTime)
+            sys.exit("BYE")
+        else:
+            data[Key[1]] = "SUCCESS"
+            data[Key[4]] = 1
+            executionTime = np.array(round((time.time() - startTime), 2))
+            with open("%sTimer_STAGE_1.time" % (ERROR_PATH), "w") as f:
+                f.write("%s" % executionTime)
+
+            os.system('echo "STAGE 1 COMPLETE"')
+
+        ## STAGE 2
+        ## python $FIRECROWN_EXAMPLES_DIR/srd_sn/sn_srd.py ${INPUT}
+        ## input_SN_sacc_file.sacc
+        sacc_path_cp = (
+            "cp " + PWD + "/" + "srd-y1-converted.sacc $FIRECROWN_EXAMPLES_DIR/srd_sn/"
+        )
+        job2 = subprocess.Popen(
+            sacc_path_cp,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            shell=True,
+        )
+        job2.wait()
+        # STAGE 3
+        # srun -n 16 --cpu-bind=cores -c 16  cosmosis   sn_only.ini -p omega_m = 0.3
+        startTime = time.time()
+        jobname = "cosmosis"
+        arg = "-p "
+        arg2 = "output.filename="
+        Vector = (
+            " "
+            + ini
+            + " "
+            + arg
+            + arg2
+            + COSMOSIS_PATH
+            + os.path.splitext(ini)[0]
+            + ".txt  --mpi"
+        )
+        with redirected_stdout(OF):
+            print("Cosmosis Input Vector:", Vector)
+        with open("%sCOSMOSIS_output_%s.log" % (ERROR_PATH, ini), "wb") as file, open(
+            "%sCOSMOSIS_output_ERROR_%s.err" % (ERROR_PATH, ini), "wb"
+        ) as file_e:
+            job3 = subprocess.Popen(
+                ["cosmosis" + Vector],
+                shell=True,
+                text=True,
+                stdout=file,
+                stderr=file_e,
+            )
+        job3.wait()
+        if job3.returncode != 0:
+            os.system('echo "STAGE 2 FAILED *** \nCheck COSMOSIS error logs"')
+            data[Key[2]] = "FAILED"
+            data[Key[4]] = 0
+            outputs = yaml.dump(data, file_yaml)
+            executionTime = np.array(round((time.time() - startTime), 2))
+            with open("%sTimer_COSMOSIS.time" % (ERROR_PATH), "w") as f:
+                f.write("%s" % executionTime)
+            sys.exit("BYE")
+        else:
+            data[Key[2]] = "SUCCESS"
+            data[Key[4]] = 1
+            executionTime = np.array(round((time.time() - startTime), 2))
+            with open("%sTimer_COSMOSIS.time" % (ERROR_PATH), "w") as f:
+                f.write("%s" % executionTime)
+            os.system('echo "STAGE 2 COMPLETE"')
+
+        # STAGE 4
+        # cosmosis-postprocess ${ini} # -o OUTDIR
+        startTime = time.time()
+        jobname = "cosmosis-postprocess"
+        arg = " -o "
+        Vector = " " + COSMOSIS_PATH + ini.replace(".ini", "*.txt") + arg + PLOT_PATH
+        with redirected_stdout(OF):
+            print("cosmosis-postprocess Input Vector:", Vector)
+        with open(
+            "%sPostProcess_output_%s.log" % (ERROR_PATH, ini), "wb"
+        ) as file_post, open(
+            "%sPostProcess_output_ERROR_%s.err" % (ERROR_PATH, ini), "wb"
+        ) as file_post_e:
+            job4 = subprocess.Popen(
+                jobname + Vector,
+                shell=True,
+                text=True,
+                stdout=file_post,
+                stderr=file_post_e,
+            )
+        job4.wait()
+        if job4.returncode != 0:
+            os.system('echo "STAGE 3 FAILED *** \nCheck PostProcess error logs"')
+            data[Key[3]] = "FAILED"
+            data[Key[4]] = 0
+            outputs = yaml.dump(data, file_yaml)
+            executionTime = np.array(round((time.time() - startTime), 2))
+            with open("%sTimer_PostProcess.time" % (ERROR_PATH), "w") as f:
+                f.write("%s" % executionTime)
+            sys.exit("BYE")
+        else:
+            data[Key[3]] = "SUCCESS"
+            data[Key[4]] = 1
+            #Attributes will be printed only in this (last) stage
+            # "XXX"
+            cosmo_params = pd.read_csv(PLOT_PATH+'/means.txt',sep=r"\s+",comment='#',header=None).T
+            cosmo_params = cosmo_params.T.set_index(0).T
+            data["FoM"] = int(FoM(PLOT_PATH+'/covmat.txt'))
+            data["Ndof"] = np.shape(HD_read)[0]
+            data["CPU_MINUTES"] = (round((time.time() - time0)/60, 2)) # in minutes
+            data["chi2"] = 22
+            data["sigint"] = 0.0
+            data["label"] = "none"
+            data["BLIND"] = 0
+            data["NWARNINGS"] = 1
+            data["w0"] = cosmo_params['cosmological_parameters--w'].iloc[0]
+            data["w0sig_marg"] = cosmo_params['cosmological_parameters--w'].iloc[1]
+            data["wa"] = cosmo_params['cosmological_parameters--wa'].iloc[0]
+            data["wasig_marg"] = cosmo_params['cosmological_parameters--wa'].iloc[1]
+            data["OM"] = cosmo_params['cosmological_parameters--omega_m'].iloc[0]
+            data["OMsig_marg"] = cosmo_params['cosmological_parameters--omega_m'].iloc[1]
+            data["w0ran"] = 0
+            data["waran"] = 0
+            data["OMran"] = 0
+            # "XXX"
+            # """
+            os.system('echo "STAGE 3 COMPLETE"')
+            os.system('echo "ALL DONE"')
+            outputs = yaml.dump(data, file_yaml)
+            executionTime = np.array(round((time.time() - startTime), 2))
+            with open("%sTimer_PostProcess.time" % (ERROR_PATH), "w") as f:
+                f.write("%s" % executionTime)
+            # print(outputs)
---
+++ README.md
@@ -0,0 +1,150 @@
+Ayan Mitra 2023
+
+********************************************************************
+********************************************************************
+
+Firecrown Wrapper Manual for running with Cosmosis for SN Cosmology
+
+********************************************************************
+********************************************************************
+
+
+Summary --------------
+The Firecrown wrapper is a standalone script which links a Supernova input
+data file to Cosmosis for dark energy parameter estimation via firecrown
+likelihood module.
+
+Why --------------
+Firecrown runs in 3 stages, including a data-prep stage into sacc format.
+The firecrown wrapper executes these 3 stages as a single task, making
+firecrown easier to use and easier to embed in pipelines.
+
+Firecrown Wrapper Flowchart --------------
+
+ SN Hubble Diagram (HD) + Covariance Matrix [Input]
+ --> Single `sacc` file
+     --> Firecrown Likelihood
+          --> Cosmosis
+	       --> DE Parameter estimation
+	           --> Plots [Output]
+
+
+Input requirements --------------
+	 Mandatory  : Input file path to HD and covariance matrix,
+	 Name of HD file and covariance matrix file, cosmosis input
+	 `ini` file.  Assumption : HD and covariance matrix files are
+	 in the same folder.
+
+	 Additional optional attributes
+	 can be seen via the help command as : 
+	 `$TD/SOFTWARE/firecrown_wrapper/dist/Firecrown_wrapper
+	 --help`
+
+   [XXX: need a more standard location for the wrapper on perlmutter]
+   
+Notes --------------
+	The wrapper can be used as : (a) a standalone unit for submitting
+	batch job in Perlmutter or, (b) can also be used with SNANA/DESC
+	TD pipeline software's function : `submit_batch_jobs.sh` for
+	submitting multiple batch job(s).
+
+Syntax --------------
+	 (a) To simply run the code as a batch job in Perlmutter the
+	 following example job script can be used as a template :
+
+	  #!/bin/bash
+	  #SBATCH -A m1727
+	  #SBATCH -C cpu
+	  #SBATCH --qos=debug
+	  #SBATCH --time=00:10:00
+	  #SBATCH --nodes=3
+	  #SBATCH --error=perl_firecrown-%j.err
+	  #SBATCH -o perl_firecrown.log
+	  #SBATCH --mail-user=ayanmitra375@gmail.com
+	  #SBATCH -J Firecrown
+
+
+	  NUM_PROCESSES=16
+	  export OMP_NUM_THREADS=4
+
+	  #Example 1
+	  # Syntax : srun -u -n ${NUM_PROCESSES}  --cpus-per-task ${OMP_NUM_THREADS} $TD/SOFTWARE/firecrown_wrapper/dist/Firecrown_wrapper [Input/Folder/HD/covariance/matrix] [HD.txt] [cov.txt] [cosmosis.ini]
+	  srun -u -n ${NUM_PROCESSES}  --cpus-per-task ${OMP_NUM_THREADS} $TD/SOFTWARE/firecrown_wrapper/dist/Firecrown_wrapper $HOME/Analysis/7_CREATE_COV/LSST_BINNED_COV_BBC_SIMDATA_PHOTOZ_1/output hubble_diagram.txt covsys_000.txt.gz sn_only.ini --summary $PWD/FIRECROWN_OUTPUT/SUMMARY.YAML -O $PWD/FIRECROWN_OUTPUT/
+
+	  # ## End of file
+
+
+	 Save the above code in a file `test.sh` and then from Perlmutter
+	 terminal submit the job as : `sbatch test.sh`
+
+
+	 *Notes : qos = `debug` can be changed to `regular`. `nodes`
+	 and time can be modified accordingly (as of now Perlmutter's
+	 maximum time limit is 12 hours). The outputs will be stored
+	 in $PWD/FIRECROWN_OUTPUT/. cosmosis input file shown here is
+	 `sn_only.ini`, consult cosmosis manual for more information.
+
+
+	 (b) To run with `submit_batch_jobs.sh` a separate Input yaml
+	 file is needed suitable for `submit_batch_jobs.sh`. An example
+	 of a test.yaml is below :
+
+
+	  ## YAML begins : CONFIG:
+	    BATCH_INFO: sbatch
+	    $SBATCH_TEMPLATES/SBATCH_Perlmutter.TEMPLATE 25
+	    JOBNAME: srun -n 2 $TD/SOFTWARE/firecrown_wrapper/dist/Firecrown_wrapper
+	    BATCH_WALLTIME: "12:00:00" FIRECROWN_INPUT_FILE:
+	    /global/cfs/cdirs/lsst/groups/TD/SN/SNANA/SURVEYS/LSST/ROOT/starterKits/firecrown+submit_batch_jobs/Cosmosis_Input_Scripts/sn_planck.ini
+	    ENV_REQUIRE: FIRECROWN_DIR FIRECROWN_EXAMPLES_DIR CSL_DIR
+	    OUTDIR: output_firecrown_sn_cmb WFITAVG:
+	   #- LSST_BINNED_COV_BBC_SIMDATA_SPECZ
+	    - LSST_BINNED_COV_BBC_SIMDATA_PHOTOZ
+	    COVOPT:  ALL NOSYS INPDIR: -
+	    /pscratch/sd/d/desctd/PIPPIN_OUTPUT/PLASTICC_COMBINED_PUBLISHED/7_CREATE_COV/LSST_BINNED_COV_BBC_SIMDATA_PHOTOZ_1/output
+	    -
+	    /pscratch/sd/d/desctd/PIPPIN_OUTPUT/PLASTICC_COMBINED_PUBLISHED/7_CREATE_COV/LSST_BINNED_COV_BBC_SIMDATA_PHOTOZ_2/output
+	  ##END_YAML
+
+
+	 Launch the job : `submit_batch_jobs.sh test.yaml`
+
+
+Outputs --------------
+	 Each successful output will produce the following three
+	 sub folders in the desired location :
+	 (1) COSMOSIS-CHAINS
+	     Contains the output chain files (name as written in input
+	     `.ini` file) and `INPUT.INFO` file which logs all input
+	     commands.
+
+	 (2) ERROR_LOGS
+	     contains the error and log files of each stages. Also
+	     contains files recording the time taken in each stages.
+
+	 (3) PLOTS
+	     contains the results of the analysis from the chain files
+	     i.e. plots and parameter estimation summary files.
+
+
+	 (4) Summary Yaml file : summarizes the outcomes of each stages
+	 : Fail or Successful. Also lists the main cosmological results
+	 (some fields are still under construction).
+
+
+Example Runtimes --------------
+
+         Running the example `sn_planck.ini` for emcee smapler with nsteps = 10, samples = 100,walkers = 24, etc. using submit_batch_jobs.sh
+	 for different number of cores and N (= MPI tasks) specification
+	 
+         Core  N. Time
+         20.   3.  4.755 hrs
+         25.   1.  2.142
+         25    2.  1.964 
+         40    1   1.810
+	 
+********************************************************************
+********************************************************************
+
+
+[1] https://sacc.readthedocs.io/en/latest/intro.html
